79f5ca9624922a9deaea44baf9f3bdbe6d885f46
frozenbug <feby.denahon@gmail.com>
2015-01-29 23:54:45 -0800

    Added Summary

--- VocabularyProject/Vocab_v2.js
+++ VocabularyProject/Vocab_v2.js
@@ -5,13 +5,17 @@ var myApp = angular.module("vocabApp",[]);
 		http.get('words_b.json').success(function (result) {
 			sc.words_success = 0;
 			sc.words_completed = 0;
-			sc.words_left = result.length;
-			sc.words=result;
+			sc.words = result;
 			sc.words_pct = 0;
+			sc.total_words = result.length;
+			sc.words_left = sc.total_words;
 			sc.getNextQn();
 		});
 		sc.getNextQn=function(){
-			var randNo = Math.round(sc.Math.random() * 100);
+			var randNo = Math.floor(sc.Math.random() * sc.words_left);
+
+			console.log(sc.words);
+			console.log(randNo);
 
 		// UI
 			$("#ans").css('visibility','hidden');
@@ -26,19 +30,31 @@ var myApp = angular.module("vocabApp",[]);
 			sc.words.splice(randNo,1);
 			sc.words_left = sc.words.length;
 			
-			//console.log(sc.word);
 		}
 		sc.validate=function(){
 		//UI
 			$("#ans").css('visibility','visible');
-			$( "#ans" ).show( 'blind', {}, 500, function() {} );
 			$("#validate").hide();
-			$("#next").show();
 			
 			if (sc.word === $("#word").val()) {
 				sc.words_success += 1;
 			}
 			sc.words_completed += 1;
 			sc.words_pct = Math.round(sc.words_success/sc.words_completed * 100);
+			
+			if (sc.words_left > 0) {
+				$("#next").show();
+			}
+			else {
+				$("#bSummary").show();
+			}
 		}
+		sc.summary=function(){
+			$("#output").hide();
+			$("#summary").show();
+		}		
 	}]);
+
+//Todo
+	//List of bad words
+	//Option to use multiple files
